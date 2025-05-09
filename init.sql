dee59e3929bcc8ba92924fbef8d0c2f36b8ff7df
arabayev <adilet.adilek@gmail.com>
2025-05-09 12:19:28 +0500

    feat(db): add ENUM and indexes

--- init.sql
+++ init.sql
@@ -1,29 +1,35 @@
+
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE users(
-    user_id UUID PRIMARY KEY,
+CREATE TYPE post_status AS ENUM ('active', 'archived', 'deleted');
+
+CREATE TABLE users (
+    user_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name VARCHAR(50) NOT NULL,
     avatar_url TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE posts(
-    post_id UUID PRIMARY KEY,
-    user_id UUID  REFERENCES users(user_id),
-    created_at TIMESTAMP NOT NULL  DEFAULT NOW(),
+CREATE TABLE posts (
+    post_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id UUID REFERENCES users(user_id) ON DELETE CASCADE,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     title VARCHAR(50) NOT NULL,
     subject VARCHAR(50),
     content TEXT,
     image_url TEXT,
-    status VARCHAR(30)
+    status post_status NOT NULL DEFAULT 'active'
 );
 
-CREATE TABLE comments(
-    comment_id UUID PRIMARY KEY,
-    post_id UUID REFERENCES posts(post_id),
+CREATE TABLE comments (
+    comment_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    post_id UUID REFERENCES posts(post_id) ON DELETE CASCADE,
     parent_id UUID REFERENCES comments(comment_id) ON DELETE CASCADE,
-    user_id  UUID REFERENCES users(user_id),
+    user_id UUID REFERENCES users(user_id) ON DELETE CASCADE,
     content TEXT,
     image_url TEXT,
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
+
+CREATE INDEX idx_comments_post_id ON comments(post_id);
+CREATE INDEX idx_posts_user_id ON posts(user_id);
